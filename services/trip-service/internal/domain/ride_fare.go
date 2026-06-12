9b05845b945aa741397e4b3c94372d762bf6f9af
mhmdhalawi <mohammadhalawi93@gmail.com>
2026-06-12 19:00:23 +0200

    feat: add route to RideFareModel and update GenerateTripFares method to include route parameter

--- services/trip-service/internal/domain/ride_fare.go
+++ services/trip-service/internal/domain/ride_fare.go
@@ -1,16 +1,18 @@
 package domain
 
 import (
+	tripTypes "ride-sharing/services/trip-service/pkg/types"
 	pb "ride-sharing/shared/proto/trip"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type RideFareModel struct {
-	ID                primitive.ObjectID `json:"id"`
-	UserID            string             `json:"user_id"`
-	PackageSlug       string             `json:"package_slug"`
-	TotalPriceInCents float64            `json:"total_price_in_cents"`
+	ID                primitive.ObjectID         `json:"id"`
+	UserID            string                     `json:"user_id"`
+	PackageSlug       string                     `json:"package_slug"`
+	TotalPriceInCents float64                    `json:"total_price_in_cents"`
+	Route             *tripTypes.OsrmApiResponse `json:"route"`
 }
 
 func (r *RideFareModel) ToProto() *pb.RideFare {
