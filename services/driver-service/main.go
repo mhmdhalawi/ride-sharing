91cdb04002b6e74889c87180eb93e2d92a651565
mhmdhalawi <mohammadhalawi93@gmail.com>
2026-06-19 18:15:28 +0200

    feat: implement DriverService with Register and Unregister RPCs, and enhance driver service with gRPC handler

--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	grpcserver "google.golang.org/grpc"
+)
+
+var GrpcAddr = ":9092"
 
 func main() {
-	log.Println("Hello world")
+	svc := NewService()
+	lis, err := net.Listen("tcp", GrpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpcserver.NewServer()
+	NewGrpcHandler(grpcServer, svc)
+
+	serverErrors := make(chan error, 1)
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("Starting gRPC server Trip service on port %s", lis.Addr().String())
+		serverErrors <- grpcServer.Serve(lis)
+	}()
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("server error: %v", err)
+
+	case sig := <-shutdown:
+		log.Printf("shutting down due to %v", sig)
+		grpcServer.GracefulStop()
+	}
+
 }
